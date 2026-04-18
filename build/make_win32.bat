175a7675d246b076ff1bc3a31cfc3c6362335164
TAV <tereshenkov@mail.ru>
2026-04-18 17:20:25 +0300

    fix: В скрипте сборки для FPC добавлена очистка выходного каталога.

--- build/make_win32.bat
+++ build/make_win32.bat
@@ -1,3 +1,6 @@
 mkdir i386-win32
+REM Очистка каталога важна, потому что в проектах одинаковые файлы main
+del i386-win32\* /q
 ppc386 @fpc-config ..\BkBasicPreprocessor\BkBasicPreprocessor.pp
+del i386-win32\* /q
 ppc386 @fpc-config ..\BkFocalPreprocessor\BkFocalPreprocessor.pp
